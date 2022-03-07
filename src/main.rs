c6540bbe2d0f4addc83873466139b374b685d02b
Linken-code <294258474@qq.com>
2022-03-07 22:00:30 +0800

    refactor: 调整文件目录结构

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use core::blockchain;
+
 fn main() {
-    println!("Hello, world!");
+    let mut bc = blockchain::BlockChain::new_blockchain();
+    bc.add_block("a->b:5btc".to_string());
+    bc.add_block("c->d:5btc".to_string());
+    for i in bc.blocks {
+        println!("digging block");
+        println!("{:#?}", i);
+    }
 }
